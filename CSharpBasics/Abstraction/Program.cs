eaacf9a224d05d08c99c948c1c4852dfeff939d6
Obinna Ezeh <95531954+Obensunday@users.noreply.github.com>
2022-05-26 10:48:51 +0100

    Add files via upload
    Added Fundamentals of Programming using C# for Beginners Learn C# Basics YouTube tutorial source code

---
+++ CSharpBasics/Abstraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Abstraction
+{
+    //Abstract class
+    abstract class Animal
+    {
+        //abstract method
+        public abstract void animalSound();
+
+        //regular method
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    class Pig : Animal //Derived class(inherited from Animal)
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee!");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig pig = new Pig();//create a pig object
+            pig.animalSound(); // call the abstract method
+            pig.sleep();//call the regular method
+        }
+    }
+}
