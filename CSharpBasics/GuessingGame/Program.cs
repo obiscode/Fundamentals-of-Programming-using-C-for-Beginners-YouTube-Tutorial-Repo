99200e6e61561275ecb4f530eb1f129289db9067
Obinna Ezeh <95531954+Obensunday@users.noreply.github.com>
2022-05-26 11:03:10 +0100

    Add files via upload

---
+++ CSharpBasics/GuessingGame/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static int guess;
+        static int target;
+        static Random randNumber = new Random();
+        static string input = "";
+        static void Game()
+        {
+            //generate a random number
+            target = randNumber.Next(10) + 1; //randNumber.Next(1,11)
+
+            //prompt the user
+            Console.WriteLine("Guess what number between 1 and 10 I am thinking of...");
+
+            //read user input
+            input = Console.ReadLine();
+
+            //feedback
+            if(int.TryParse(input, out guess))
+            {
+                if (guess == target)
+                {
+                    Console.WriteLine("Congratulations! You guessed " + guess +
+                        " and the number I was thinking of was " + target + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Too bad. You guessed " + guess + " and" +
+                        " the number I was thinking of was " + target + ".");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number.");
+                Console.WriteLine("Press enter to continue...\n");
+                Console.ReadLine();
+                Game();
+            }          
+            
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Game();
+            }
+           
+        }
+    }
+}
---
+++ CSharpBasics/Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inheritance
+{
+    sealed class Vehicle //Parent class or Base class
+    {
+        public string brand = "Lexus"; //vehicle field
+        public void Start()//Vehicle method
+        {
+            Console.WriteLine("Engine starting...");
+        }
+    }
+
+    class Car : Vehicle// the child class or derived class
+    {
+        public string modelName = "RX 350";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create myCar object
+            Car myCar = new Car();
+
+            //call the start() method from the Vehicle class on myCar object
+            myCar.Start();
+
+            Console.WriteLine(myCar.brand);
+        }
+    }
+}
