37bfd17441ecd755b862fcc9d5364f871ca55d21
Obinna Ezeh <95531954+Obensunday@users.noreply.github.com>
2022-05-26 11:37:06 +0100

    Add files via upload

---
+++ CSharpBasics/WhileLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;//i = i + 1;
+            //}
+
+            //do/while loop
+            int k = 0;
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+            } while (k < 10);
+
+        }
+    }
+}
